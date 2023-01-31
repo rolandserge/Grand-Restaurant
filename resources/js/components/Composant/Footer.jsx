ea7667dec3b5624ca811b3298af2f47e892839bf
sergeroland <sergeroland.komenan@gmail.com>
2023-01-31 16:23:31 +0000

    main

--- resources/js/components/Composant/Footer.jsx
+++ resources/js/components/Composant/Footer.jsx
@@ -4,34 +4,46 @@ import Fb from "../../Assets/Fbook.png"
 import Twitter from "../../Assets/twt.png"
 import Insta from "../../Assets/Insta.png"
 import Email from "../../Assets/E-mail.png"
+import Telephone from "../../Assets/appel.png"
+import Localisation from "../../Assets/localisation.png"
 
 const Footer = () => {
     return (
-        <div>
-            <div>
-                <div>
-
-                    <div>
-                        <p>GRAND</p>
-                        <p>RESTAURANT</p>
+        <div className='footer_center'>
+            <div className='footer_container'>
+                <div className="card titre">
+                    <div className='logo'>
+                        <p className='grand'>GRAND</p>
+                        <p className='resto'>RESTAURANT</p>
                     </div>
-                    <div>
+                    <div className='description'>
+                        <p>
                         Lorem ipsum dolor sit amet, consectetur
                         adipisicing elit. Aspernatur, at?
                         Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet.
+                        </p>
                     </div>
-                    <div>
-                        <img src={Fb} alt="" />
-                        <img src={Insta} alt="" />
-                        <img src={Twitter} alt="" />
-                        <img src={Email} alt="" />
+                    <div className='compte_infos'>
+                        <div className='image_card'>
+                            <img src={Fb} alt="" />
+                        </div>
+                        <div className='image_card'>
+                            <img src={Insta} alt="" />
+                        </div>
+                        <div className='image_card'>
+                            <img src={Twitter} alt="" />
+                        </div>
+                        <div className='image_card'>
+                            <img src={Email} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div>
+                <div className="card quicks">
                     <div>
-                        <p>Quicks Link</p>
+                        <p className='quick'>Quicks Link</p>
                     </div>
-                    <div>
+                    <div className='quick_menu'>
                         <p>Menu</p>
                         <p>Services</p>
                         <p>Restauration</p>
@@ -39,31 +51,31 @@ const Footer = () => {
                         <p>Order Food</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className="card working">
+                    <div className='work'>
                         <p>Woorking Hours</p>
                     </div>
-                    <div>
+                    <div className='horaire'>
                         <p>Lundi - Vendredi ---- 09 H a 22 H</p>
                         <p>Samedi ----- 10 H 30 20</p>
                         <p>Dimanche ----- 12 H a 19 H</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className="card coordonner">
+                    <div className='finds'>
                         <p>Find Us</p>
                     </div>
-                    <div>
+                    <div className='mes_contacts'>
                         <div>
                             <img src={Email} alt="" />
                             <p>GrandRestaurant@gmail.com</p>
                         </div>
                         <div>
-                            <img src="" alt="" />
+                            <img src={Telephone} alt="" />
                             <p>+ 225 07 04 21 91 95</p>
                         </div>
                         <div>
-                            <img src="" alt="" />
+                            <img src={Localisation} alt="" />
                             <p>Esatic bd de Marseille</p>
                         </div>
                     </div>
